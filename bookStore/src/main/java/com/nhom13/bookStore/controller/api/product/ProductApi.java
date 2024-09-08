ed65a96c7770de42b2e043d31b275ec4f5cb436f
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-08 07:49:02 +0700

    chung

--- bookStore/src/main/java/com/nhom13/bookStore/controller/api/product/ProductApi.java
+++ bookStore/src/main/java/com/nhom13/bookStore/controller/api/product/ProductApi.java
@@ -1,6 +1,7 @@
 package com.nhom13.bookStore.controller.api.product;
 
 import com.nhom13.bookStore.dto.ApiResponse;
+import com.nhom13.bookStore.dto.product.ProductDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/product")
 public class ProductApi {
     @PostMapping
-    public ResponseEntity<ApiResponse<String>> createProduct(){
+    public ResponseEntity<ApiResponse<String>> createProduct(@ModelAttribute @Valid ProductDTO productDTO){
+
         return  ResponseEntity.ok(new ApiResponse<>());
     }
     @PatchMapping
