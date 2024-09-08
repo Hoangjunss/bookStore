825d3681979c4e8ced79af7e7d481e9e970a32fd
trungkien <145746252+allpedog@users.noreply.github.com>
2024-09-08 10:33:51 +0700

    kiên

--- bookStore/src/main/java/com/nhom13/bookStore/repository/cart/CartDetailRepository.java
+++ bookStore/src/main/java/com/nhom13/bookStore/repository/cart/CartDetailRepository.java
@@ -3,6 +3,9 @@ package com.nhom13.bookStore.repository.cart;
 
 import com.nhom13.bookStore.model.cart.CartDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface CartDetailRepository extends JpaRepository<CartDetails,Integer> {
+        List<CartDetails> findByIdCart(Integer idCart);
 }
